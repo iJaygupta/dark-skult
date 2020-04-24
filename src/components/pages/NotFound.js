3c4a8efa617c748d9b2e4f660403672b3a8354d7
Jay Gupta <jayguptazzz@gmail.com>
2020-04-24 11:56:12 +0530

    page not found card added

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
- export default function DisplayMessage(props) {
+export default function DisplayMessage(props) {
 
-    if(!props.hasOwnProperty('message')){
-      return(<div>404 Error, Page Not Found!</div>);
-    } else  {
-      return(<div>{props.message}</div>);
-    }
+  if (!props.hasOwnProperty('message')) {
+    return (
+      <div>
+        <div className="card">
+          <div className="card-body" >
+            <h2 className="font-weight-bold" style={{ "textAlign": "center" }}> 404 Error, Page Not Found!</h2>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {props.message}
+      </div>
+    );
+  }
 }
