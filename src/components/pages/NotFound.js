080055612e5a5ca65a5fe51b6226914a7895a3ea
AjitKapoor <ajitkapoor86@gmail.com>
2020-04-24 11:46:51 +0530

    page not found updated

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -3,7 +3,7 @@ import React from 'react';
  export default function DisplayMessage(props) {
 
     if(!props.hasOwnProperty('message')){
-      return(<div>Not Found</div>);
+      return(<div>404 Error, Page Not Found!</div>);
     } else  {
       return(<div>{props.message}</div>);
     }
