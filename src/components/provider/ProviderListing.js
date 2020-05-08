bcc305ea0015330adfb275e38e1f71d5817bf077
Jay Gupta <jayguptazzz@gmail.com>
2020-05-08 23:05:02 +0530

    merge changes

--- src/components/provider/ProviderListing.js
+++ src/components/provider/ProviderListing.js
@@ -1,6 +1,6 @@
 
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom'
 import $ from "jquery";
 
